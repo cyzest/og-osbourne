2427967b3fd10f90dfd8a597c6a0f98d96ea3c4b
cyzest <cyzest@nate.com>
2018-10-23 23:06:13 +0900

    Maven 기반 프로젝트 설정 및 Lambda Handler 구성

---
+++ src/test/java/com/cyzest/ogosbourne/handler/ConvertRequestHandlerTest.java
@@ -0,0 +1,14 @@
+package com.cyzest.ogosbourne.handler;
+
+import org.junit.jupiter.api.Test;
+
+public class ConvertRequestHandlerTest {
+
+    @Test
+    public void handleRequestTest() {
+
+
+
+    }
+
+}
